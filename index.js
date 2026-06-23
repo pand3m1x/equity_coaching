63e288b72469be960314f1da00ed7b933dfb0ca5
TheRedQueen <pand3m1x@gmail.com>
2026-06-23 17:43:09 -0400

    made adjustments to form, enabled Netlify form recognition

--- index.js
+++ index.js
@@ -5,3 +5,12 @@ window.addEventListener("scroll", () => {
     const nav = document.getElementById("top-navbar");
     nav.classList.toggle("scrolled", window.scrollY > 50);
 });
+
+const form = document.getElementById("contactForm");
+const successMessage = document.getElementById("successMessage");
+
+form.addEventListener("submit", function () {
+    setTimeout(() => {
+        successMessage.style.display = "block";
+    }, 500);
+});
