1108737c25ad63c924073aa4ec47dfee17759315
TheRedQueen <pand3m1x@gmail.com>
2026-06-23 18:00:00 -0400

    form works perfectly, connecting client email. Logo, Workshop image, and partners have alt text

--- index.js
+++ index.js
@@ -6,11 +6,11 @@ window.addEventListener("scroll", () => {
     nav.classList.toggle("scrolled", window.scrollY > 50);
 });
 
-const form = document.getElementById("contactForm");
-const successMessage = document.getElementById("successMessage");
+// const form = document.getElementById("contactForm");
+// const successMessage = document.getElementById("successMessage");
 
-form.addEventListener("submit", function () {
-    setTimeout(() => {
-        successMessage.style.display = "block";
-    }, 500);
-});
+// form.addEventListener("submit", function () {
+//     setTimeout(() => {
+//         successMessage.style.display = "block";
+//     }, 500);
+// });
